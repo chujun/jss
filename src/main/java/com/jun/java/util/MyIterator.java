79c971e767e8215488fb40c2640a033380dcf60e
jun.chu <1571970439@qq.com>
2020-06-03 20:45:44 +0800

    feat(myiterator): add MyIterator

---
+++ src/main/java/com/jun/java/util/MyIterator.java
@@ -0,0 +1,58 @@
+package com.jun.java.util;
+
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.function.Consumer;
+
+/**
+ * @author chujun
+ * @date 2020-06-03
+ * @see java.util.Iterator
+ */
+public interface MyIterator<E> {
+    /**
+     * jfm
+     *
+     * @return {@code true} if the iteration has more elements
+     */
+    boolean hasNext();
+
+    /**
+     * Returns the next element in the iteration.
+     *
+     * @return the next element in the iteration
+     * @throws NoSuchElementException if the iteration has no more elements
+     */
+    E next();
+
+    /**
+     * jfm
+     *
+     * @throws UnsupportedOperationException if the {@code remove}
+     *                                       operation is not supported by this iterator
+     * @throws IllegalStateException         if the {@code next} method has not
+     *                                       yet been called, or the {@code remove} method has already
+     *                                       been called after the last call to the {@code next}
+     *                                       method
+     * @implSpec The default implementation throws an instance of
+     * {@link UnsupportedOperationException} and performs no other action.
+     */
+    default void remove() {
+        throw new UnsupportedOperationException("remove");
+    }
+
+
+    /**
+     * jfm
+     *
+     * @param action The action to be performed for each element
+     * @throws NullPointerException if the specified action is null
+     * @since 1.8
+     */
+    default void forEachRemaining(Consumer<? super E> action) {
+        Objects.requireNonNull(action);
+        while (hasNext()) {
+            action.accept(next());
+        }
+    }
+}
